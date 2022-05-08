80d008501ab8b5e4cebaf630c497897ce72c417c
Nelutato <purepeper@gmail.com>
2022-05-08 20:55:37 +0200

    prettier

--- app/Http/Controllers/Auth/Admin/DasboardController.php
+++ app/Http/Controllers/Auth/Admin/DasboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Recepie;
 use Illuminate\Http\Request;
 
 class DasboardController extends Controller
@@ -11,8 +12,10 @@ class DasboardController extends Controller
     public function dashboard()
     {
         $users = User::all();
+        $recepies = Recepie::all();
         return view('auth.admin.dashboard',[
-            'users'=>$users,
+            'users' => $users,
+            'recepies' => $recepies,
         ]);
     }
     public function deleteUser($id)
